01642702268e046800b7c754418461344f249087
khalil <khalil.haouas@gmail.com>
2024-11-06 22:18:26 +0100

    update

--- src/components/car-form/CarCard.tsx
+++ src/components/car-form/CarCard.tsx
@@ -1,4 +1,4 @@
-import { FaCheckCircle, FaUserTie } from "react-icons/fa";
+import { FaCarSide, FaCheckCircle, FaUserTie } from "react-icons/fa";
 import {
   Card,
   CardContent,
@@ -17,22 +17,51 @@ import { useSelector } from "react-redux";
 import { getAllRentCars } from "../../../feature/reducers/carRentSlice";
 import { useState } from "react";
 import { CiCircleInfo } from "react-icons/ci";
+import { WiFog } from "react-icons/wi";
+
 
 interface ICarCardProps {
   availableCars: ICarRent[];
 }
 
 const CarCard = ({ availableCars }: ICarCardProps) => {
-  const [detailsVisibility, setDetailsVisibility] = useState<{ [key: string]: boolean }>({});
+  const [loading, setLoading] = useState(false);
+  const [detailsVisibility, setDetailsVisibility] = useState<{
+    [key: string]: boolean;
+  }>({});
   const rentCars = useSelector(getAllRentCars);
   const router = useRouter();
 
   const toggleDetails = (carId: string) => {
-    setDetailsVisibility(prevState => ({
+    setDetailsVisibility((prevState) => ({
       ...prevState,
       [carId]: !prevState[carId],
     }));
   };
+
+  const handleSelectCar = (carId: string) => {
+    setLoading(true);
+    localStorage.setItem("carRentId", carId || "");
+    setTimeout(() => {
+      router.push(`/fahrzeugvermietung/${carId}`);
+      setLoading(false);
+    }, 3000);
+  };
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className=" flex items-center gap-5 border-t-transparent rounded-full antialiased">
+         <div className=" flex items-center gap-1 text-3xl text-orange-400">
+            <WiFog className="animate-pulse"/><WiFog className="animate-pulse" /><WiFog className="animate-pulse"/>
+         </div> 
+        <div>
+        <FaCarSide className="text-orange-400 text-5xl sm:text-[8rem] animate-bounce" />
+        </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -78,7 +107,9 @@ const CarCard = ({ availableCars }: ICarCardProps) => {
                       </p>
                       <p className="flex gap-3 items-center">
                         <FaCheckCircle className="text-green-500 w-3 h-3" />
-                        <span className="cardInfoSell">Basic-Schutzpaket inklusive</span>
+                        <span className="cardInfoSell">
+                          Basic-Schutzpaket inklusive
+                        </span>
                       </p>
                     </div>
 
@@ -99,20 +130,19 @@ const CarCard = ({ availableCars }: ICarCardProps) => {
                 </CardContent>
 
                 <CardFooter className="pt-6">
-                  <div
-                    onClick={() => {
-                      localStorage.setItem("carRentId", car._id || "");
-                      router.push(`/fahrzeugvermietung/${car._id}`);
-                    }}
-                    className="text-white flex flex-col gap-4"
-                  >
+                  <div className="text-white flex flex-col gap-4">
                     <p>
                       <span className="cardInfoSell">
                         <TbManualGearboxFilled className="cardInfoSellIcon" />
                         {car.carPrice} $ / Tag
                       </span>
                     </p>
-                    <button className="bg-orange-500 px-4 rounded-md font-semibold hover:bg-orange-600 transition-colors py-2">
+                    <button
+                      onClick={() => {
+                        handleSelectCar(car._id!);
+                      }}
+                      className="bg-orange-500 px-4 rounded-md font-semibold hover:bg-orange-600 transition-colors py-2"
+                    >
                       Auswählen
                     </button>
                   </div>
@@ -124,8 +154,7 @@ const CarCard = ({ availableCars }: ICarCardProps) => {
                   detailsVisibility[car._id!] ? "flex" : "hidden"
                 }`}
               >
-            
-               <div className="bg-slate-300 px-2 py-4 w-full flex flex-col gap-3 rounded-t-lg">
+                <div className="bg-slate-300 px-2 py-4 w-full flex flex-col gap-3 rounded-t-lg">
                   <h2 className="md:font-bold text-xs md:text-sm">
                     Vollständige Fahrzeugdaten (beziehen sich auf das oben
                     gezeigte Beispielfahrzeug)
@@ -149,51 +178,56 @@ const CarCard = ({ availableCars }: ICarCardProps) => {
                     </span>
                   </div>
                 </div>
-              
-              
               </div>
-              <div className={` p-4 rounded-lg transition-all duration-500 ease-in-out overflow-hidden ${
+              <div
+                className={` p-4 rounded-lg transition-all duration-500 ease-in-out overflow-hidden ${
                   detailsVisibility[car._id!] ? "flex" : "hidden"
-                }`}>
-              <div className=" grid grid-cols-1 sm:grid-cols-2 w-full gap-4">
-                    <div className="bg-slate-300 border-2 px-2 py-4 flex flex-col gap-2 sm:col-span-1">
-                        <div className=" px-4">
-                        <span className=" text-red-500 uppercase font-semibold text-xs">Inclusive</span>
-                        <h2 className=" font-bold">Kilometer</h2>
-                        </div>
-                       <div className=" flex flex-col gap-2">
-                       <div className=" flex items-center gap-2">
+                }`}
+              >
+                <div className=" grid grid-cols-1 sm:grid-cols-2 w-full gap-4">
+                  <div className="bg-slate-300 border-2 px-2 py-4 flex flex-col gap-2 sm:col-span-1">
+                    <div className=" px-4">
+                      <span className=" text-red-500 uppercase font-semibold text-xs">
+                        Inclusive
+                      </span>
+                      <h2 className=" font-bold">Kilometer</h2>
+                    </div>
+                    <div className=" flex flex-col gap-2">
+                      <div className=" flex items-center gap-2">
                         <FaCheckCircle className="text-green-500 w-3 h-3" />
                         <p>300 km inklusive</p>
-                        </div>
-                        <p>Zusätzliche Kilometer: EUR 0.35/km</p>
-                       </div>
-
+                      </div>
+                      <p>Zusätzliche Kilometer: EUR 0.35/km</p>
                     </div>
-                    <div className="bg-slate-300 border-2 sm:col-span-1 px-2 py-4 rounded-b-lg">
+                  </div>
+                  <div className="bg-slate-300 border-2 sm:col-span-1 px-2 py-4 rounded-b-lg">
                     <div className=" px-4">
-                        <span className=" text-red-500 uppercase font-semibold text-xs">Inclusive</span>
-                        <h2 className=" font-bold">Basic-Schutzpaket</h2>
-                        </div>
-                        <div className=" text-xs font-bold px-4 mt-3">
-                        Selbstbeteiligung:950,00 €
-                        </div>
-                        <div>
-                        <div className=" flex items-center gap-2 mt-5 px-4">
+                      <span className=" text-red-500 uppercase font-semibold text-xs">
+                        Inclusive
+                      </span>
+                      <h2 className=" font-bold">Basic-Schutzpaket</h2>
+                    </div>
+                    <div className=" text-xs font-bold px-4 mt-3">
+                      Selbstbeteiligung:950,00 €
+                    </div>
+                    <div>
+                      <div className=" flex items-center gap-2 mt-5 px-4">
                         <FaCheckCircle className="text-green-500 w-3 h-3" />
                         <p>Vollkaskoschutz</p>
-                        </div>
-                        <div className=" flex items-center gap-2 px-4">
+                      </div>
+                      <div className=" flex items-center gap-2 px-4">
                         <FaCheckCircle className="text-green-500 w-3 h-3" />
                         <p>Diebstahlschutz</p>
-                        </div>
-
-                        </div>
-                        <div className=" flex items-start px-4 mt-6 gap-2">
-                        <CiCircleInfo className=" w-6 h-6"/>
-                        <p>Sie können Ihr Schutzpaket erweitern, nachdem Sie das Fahrzeug ausgewählt haben</p>
-                        </div>
+                      </div>
                     </div>
+                    <div className=" flex items-start px-4 mt-6 gap-2">
+                      <CiCircleInfo className=" w-6 h-6" />
+                      <p>
+                        Sie können Ihr Schutzpaket erweitern, nachdem Sie das
+                        Fahrzeug ausgewählt haben
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </Card>
