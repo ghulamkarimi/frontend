9aad186cb96b26f8476854bd3e8e0d35dda8eb7b
khalil <khalil.haouas@gmail.com>
2024-11-11 12:27:21 +0100

    lastUpdate

--- src/app/fahrzeugvermietung/[id]/page.tsx
+++ src/app/fahrzeugvermietung/[id]/page.tsx
@@ -1,83 +1,90 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import ImageGallery from "react-image-gallery";
-import Lightbox from "yet-another-react-lightbox";
 import "react-image-gallery/styles/css/image-gallery.css";
 import "yet-another-react-lightbox/styles.css";
-import { useDispatch } from "react-redux";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import "react-image-gallery/styles/css/image-gallery.css";
+import { useSelector } from "react-redux";
 import { RootState } from "../../../../feature/store/store";
 import { getRentCarById } from "../../../../feature/reducers/carRentSlice";
 
-type ImageItem = {
-    original: string;
-    thumbnail: string;
-};
-
 const Page = () => {
-  const {id:carRentId} = useParams()
-    const getOneCar = useSelector((state: RootState) => getRentCarById(state, carRentId! as string));
+  const { id: carRentId } = useParams();
+  const getOneCar = useSelector((state: RootState) =>
+    getRentCarById(state, carRentId! as string)
+  );
+  const [pickupDate, setPickupDate] = useState<string | null>(null);
+  const [pickupTime, setPickupTime] = useState<string | null>(null);
+  const [returnDate, setReturnDate] = useState<string | null>(null);
+  const [returnTime, setReturnTime] = useState<string | null>(null);
+  const [pickupLocation, setPickupLocation] = useState<string | null>(null);
+  const [age, setAge] = useState<string | null>(null);
 
-    console.log("carRentId",carRentId)
-    const dispatch = useDispatch()
-    const [isOpen, setIsOpen] = useState(false);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [images, setImages] = useState<ImageItem[]>([]);
+  useEffect(() => {
+    // Werte aus localStorage abrufen und im State speichern
+    setPickupDate(localStorage.getItem("pickupDate"));
+    setPickupTime(localStorage.getItem("pickupTime"));
+    setReturnDate(localStorage.getItem("returnDate"));
+    setReturnTime(localStorage.getItem("returnTime"));
+    setPickupLocation(localStorage.getItem("pickupLocation"));
+    setAge(localStorage.getItem("age"));
+  }, []);
 
-    useEffect(() => {
-        console.log("getOneCar:", getOneCar); 
-    
-     
-        if (getOneCar?.carImage) {
-          
-            setImages(
-                Array(4).fill(getOneCar.carImage).map((img) => ({
-                    original: img,
-                    thumbnail: img,
-                }))
-            );
-        }
-      
-    }, [getOneCar , dispatch ,carRentId]);
 
-    const handleImageClick = (index: number) => {
-        setCurrentIndex(index);
-        setIsOpen(true); 
-    };
 
-  
-    if (!getOneCar || images.length === 0) {
-        return <div>Keine Bilder verfügbar</div>;
-    }
+  const formattedPickupDate = pickupDate
+    ? new Date(pickupDate).toLocaleDateString()
+    : "Datum nicht verfügbar";
+  const formattedReturnDate = returnDate
+    ? new Date(returnDate).toLocaleDateString()
+    : "Datum nicht verfügbar";
+  const formattedPickupTime = pickupTime || "Zeit nicht verfügbar";
+  const formattedReturnTime = returnTime || "Zeit nicht verfügbar";
 
-    return (
-        <div>
-            <div className="w-1/2">
-                <ImageGallery
-                    items={images}
-                    showNav={false} 
-                    showFullscreenButton={false} 
-                    showPlayButton={false}
-                    onClick={(event) => {
-                        const clickedIndex = images.findIndex(image => image.original === (event.target as HTMLImageElement).src);
-                        handleImageClick(clickedIndex);
-                    }}
-                />
-
-             
-                {isOpen && (
-                    <Lightbox
-                        slides={images.map((image) => ({ src: image.original }))}
-                        open={isOpen}
-                        index={currentIndex}
-                        close={() => setIsOpen(false)}
-                    />
-                )}
-            </div>
+  return (
+    <div className=" m-2">
+      <div className=" px-2 w-full flex flex-col md:flex-row items-center gap-2">
+        <div className=" bg-white px-2 py-4 rounded-lg w-full md:w-1/3 h-[10rem]">
+          <div className=" flex gap-2 items-center px-2">
+            <span className=" bg-gray-400 px-2 py-1 text-white rounded-br-xl rounded-tl-xl">
+              1
+            </span>
+            <h2 className=" uppercase">MietOrt</h2>
+          </div>
+          <div className=" flex items-center justify-between mt-2 px-2">
+            <p>Abholung</p>
+            <p>Rückgabe</p>
+          </div>
+          <div className=" mt-1 px-2 flex items-center justify-between">
+            <p>{pickupLocation}</p>
+            <p>{formattedReturnDate}</p>
+          </div>
+          <div className=" mt-1 px-2 w-full flex items-center justify-between">
+            <p >
+              {formattedPickupDate} {formattedPickupTime}
+            </p>
+            <p className=" md:text-center">
+              {formattedReturnDate} {formattedReturnTime}
+            </p>
+          </div>
+          <div className=" px-2">Age:{age}</div>
         </div>
-    );
+        <div className="bg-white px-2 py-4 rounded-lg w-full md:w-1/4 h-[10rem]">
+          <div className=" flex gap-2 items-center px-2">
+            <span className=" bg-gray-400 px-2 py-1 text-white rounded-br-xl rounded-tl-xl">
+              2
+            </span>
+            <h2 className=" uppercase">Fahrzeug</h2>
+          </div>
+          <div className=" px-2 mt-3">
+            <p>{getOneCar.carName}</p>
+            <p>Gesamt price</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
