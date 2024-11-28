0f21000d1d771a2c4a28bc377388e81bda94ef94
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-11-28 12:11:49 +0100

    Enhance PasswordComponent with password visibility toggle and improve validation messages

--- src/components/userInfos/PasswordComponent.tsx
+++ src/components/userInfos/PasswordComponent.tsx
@@ -5,10 +5,16 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../feature/store/store";
 import { NotificationService } from "../../../service/NotificationService";
 import { changePasswordApi } from "../../../feature/reducers/userSlice";
-import { AiOutlineLock } from "react-icons/ai";
+import { AiOutlineLock, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
 const PasswordComponent = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+  const [showPassword, setShowPassword] = React.useState({
+    newPassword: false,
+    confirmPassword: false,
+  });
 
   const passwordStrengthRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
 
@@ -23,20 +29,20 @@ const PasswordComponent = () => {
         .email("Ungültige E-Mail-Adresse")
         .required("E-Mail ist erforderlich"),
       newPassword: Yup.string()
+        .required("Neues Passwort ist erforderlich")
         .matches(
           passwordStrengthRegex,
-          "Das Passwort muss mindestens 8 Zeichen lang sein und mindestens einen Großbuchstaben, einen Kleinbuchstaben und eine Zahl enthalten."
-        )
-        .required("Neues Passwort ist erforderlich"),
+          "Mindestens 8 Zeichen, Groß-/Kleinbuchstaben und Zahl erforderlich."
+        ),
       confirmPassword: Yup.string()
-        .oneOf([Yup.ref("newPassword")], "Passwörter stimmen nicht überein")
-        .required("Passwort-Bestätigung ist erforderlich"),
+        .required("Passwort-Bestätigung ist erforderlich")
+        .oneOf([Yup.ref("newPassword")], "Passwörter stimmen nicht überein"),
     }),
     onSubmit: async (values, { resetForm }) => {
       try {
-        // Dispatch an Redux-Thunk
         await dispatch(changePasswordApi(values)).unwrap();
         NotificationService.success("Passwort erfolgreich geändert!");
+        router.push("/login");
         resetForm();
       } catch (error: any) {
         NotificationService.error(
@@ -46,8 +52,12 @@ const PasswordComponent = () => {
     },
   });
 
+  const togglePasswordVisibility = (field: "newPassword" | "confirmPassword") => {
+    setShowPassword((prev) => ({ ...prev, [field]: !prev[field] }));
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-blue-50 to-blue-100">
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-50 to-blue-200">
       <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
         <div className="text-center mb-6">
           <AiOutlineLock className="text-6xl text-blue-500 mx-auto mb-2" />
@@ -57,56 +67,73 @@ const PasswordComponent = () => {
           </p>
         </div>
         <form onSubmit={formik.handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
+          {/* Email Field */}
+          <div className="mb-6">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               E-Mail
             </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              placeholder="Ihre E-Mail-Adresse"
-              className={`mt-1 block w-full px-4 py-2 border rounded-md shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${formik.touched.email && formik.errors.email
-                  ? "border-red-500"
-                  : "border-gray-300"
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ihre E-Mail-Adresse"
+                className={`mt-2 block w-full px-4 py-3 border rounded-lg shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500"
+                    : "border-gray-300"
                 }`}
-            />
-            {formik.touched.email && formik.errors.email && (
-              <p className="text-red-500 text-xs mt-1">{formik.errors.email}</p>
-            )}
+              />
+            </div>
+            <div className="h-6">
+              {formik.touched.email && formik.errors.email && (
+                <p className="text-red-500 text-sm">{formik.errors.email}</p>
+              )}
+            </div>
           </div>
-          <div className="mb-4">
-            <label
-              htmlFor="newPassword"
-              className="block text-sm font-medium text-gray-700"
-            >
+
+          {/* New Password Field */}
+          <div className="mb-6">
+            <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
               Neues Passwort
             </label>
-            <input
-              type="password"
-              id="newPassword"
-              name="newPassword"
-              value={formik.values.newPassword}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              placeholder="Neues Passwort"
-              className={`mt-1 block w-full px-4 py-2 border rounded-md shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${formik.touched.newPassword && formik.errors.newPassword
-                  ? "border-red-500"
-                  : "border-gray-300"
+            <div className="relative">
+              <input
+                type={showPassword.newPassword ? "text" : "password"}
+                id="newPassword"
+                name="newPassword"
+                value={formik.values.newPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Neues Passwort"
+                className={`mt-2 block w-full px-4 py-3 border rounded-lg shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${
+                  formik.touched.newPassword && formik.errors.newPassword
+                    ? "border-red-500"
+                    : "border-gray-300"
                 }`}
-            />
-            {formik.touched.newPassword && formik.errors.newPassword && (
-              <p className="text-red-500 text-xs mt-1">
-                {formik.errors.newPassword}
-              </p>
-            )}
+              />
+              <span
+                onClick={() => togglePasswordVisibility("newPassword")}
+                className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+              >
+                {showPassword.newPassword ? (
+                  <AiOutlineEyeInvisible className="text-gray-500" />
+                ) : (
+                  <AiOutlineEye className="text-gray-500" />
+                )}
+              </span>
+            </div>
+            <div className="h-5">
+              {formik.touched.newPassword && formik.errors.newPassword && (
+                <p className="text-red-500 text-sm">{formik.errors.newPassword}</p>
+              )}
+            </div>
           </div>
+
+          {/* Confirm Password Field */}
           <div className="mb-6">
             <label
               htmlFor="confirmPassword"
@@ -114,37 +141,51 @@ const PasswordComponent = () => {
             >
               Passwort bestätigen
             </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={formik.values.confirmPassword}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              placeholder="Neues Passwort bestätigen"
-              className={`mt-1 block w-full px-4 py-2 border rounded-md shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${formik.touched.confirmPassword && formik.errors.confirmPassword
-                  ? "border-red-500"
-                  : "border-gray-300"
+            <div className="relative">
+              <input
+                type={showPassword.confirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                name="confirmPassword"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Neues Passwort bestätigen"
+                className={`mt-2 block w-full px-4 py-3 border rounded-lg shadow-sm text-gray-900 focus:ring-blue-500 focus:border-blue-500 ${
+                  formik.touched.confirmPassword && formik.errors.confirmPassword
+                    ? "border-red-500"
+                    : "border-gray-300"
                 }`}
-            />
-            {formik.touched.confirmPassword &&
-              formik.errors.confirmPassword && (
-                <p className="text-red-500 text-xs mt-1">
-                  {formik.errors.confirmPassword}
-                </p>
+              />
+              <span
+                onClick={() => togglePasswordVisibility("confirmPassword")}
+                className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+              >
+                {showPassword.confirmPassword ? (
+                  <AiOutlineEyeInvisible className="text-gray-500" />
+                ) : (
+                  <AiOutlineEye className="text-gray-500" />
+                )}
+              </span>
+            </div>
+            <div className="h-5">
+              {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+                <p className="text-red-500 text-sm">{formik.errors.confirmPassword}</p>
               )}
+            </div>
           </div>
-          <div className="flex justify-between items-center">
+
+          {/* Buttons */}
+          <div className="flex justify-between items-center mt-6">
             <button
               type="button"
               onClick={formik.handleReset}
-              className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition-all"
+              className="px-6 py-3 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition-all"
             >
               Abbrechen
             </button>
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all"
+              className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all"
             >
               Ändern
             </button>
